f2216f6c2fc5d8590b405d773e4f7d0a89164f86
kstern00 <104535962+kstern00@users.noreply.github.com>
2023-11-20 17:07:40 -0600

    Update Courses.java

--- src/default_package/Courses.java
+++ src/default_package/Courses.java
@@ -1,16 +1,25 @@
-
+package full_stack.demo;
+import jakarta.persistence.*;
 
 public class Courses {
+	//@Id
+  //  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@Column(name = "courseID")
 	public String courseID;
+//	@Column(name = "courseTitle")
 	public String courseTitle;
+//	@Column(name = "credit")
 	public double credit;
+//	@Column(name = "department")
 	public String department;
+	public String finalGrade;
 	
-	public void Courses(String cID, String cTitle, double cr, String dep) {
+	public void Courses(String cID, String cTitle, double cr, String dep, String fg) {
 		this.courseID = cID;
 		this.courseTitle = cTitle;
 		this.credit = cr;
 		this.department = dep;
+		this.finalGrade = fg;
 	}
 	
 	public String getTitle() {
@@ -27,20 +36,27 @@ public class Courses {
 	public String getDepartment() {
 		return this.department;
 	}
+
+	public String getCourseGrade(){
+		return finalGrade;
+	}
 	
-	public void Courses(String cID, String cTitle, double cr) {
+	public void Courses(String cID, String cTitle, double cr, String fg) {
 		this.courseID = cID;
 		this.courseTitle = cTitle;
 		this.credit = cr;
 		this.department = "NA";
+		this.finalGrade = fg;
 		
 	}
 	
 	public String toString() {
 		String str = "Course ID: " + getCourseID();
 		str = str+ ", Course Title: " + getTitle() +"\n";
+		str = str+ ", Course Grade " + getCourseGrade() +"\n";
 		return str;
 	}
 	
 
 }
+
